11768852b9b39ddab402e775cebaf371df90e8ea
Jason Ho <dizzyflames@hotmail.com>
2022-04-01 22:15:11 -0400

    started on webtoon bridge

---
+++ bridges/WebtoonBridge.php
@@ -0,0 +1,10 @@
+<?php
+    // work in progress
+	class WebtoonBridge extends BridgeAbstract {
+        const URI = 'https://www.webtoon.xyz/';
+        const name = 'webtoon';
+        const description = 'returns all of the chapters';
+        const PARAMETERS = array ( array(
+            'name' => 'name'
+        ));
+    }
