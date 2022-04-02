17dfb259a12d21cff9dc32cce0ac9134ca53402e
Jason Ho <dizzyflames@hotmail.com>
2022-04-01 23:22:23 -0400

    commented out html GET

--- bridges/WebtoonBridge.php
+++ bridges/WebtoonBridge.php
@@ -17,7 +17,7 @@ class WebtoonBridge extends BridgeAbstract {
 
 	public function collectData() {
 		$item = array(); // Create an empty item
-        $html = getSimpleHTMLDOM(self::URI + '//read//' + $this->getInput('n'));
+        //$html = getSimpleHTMLDOM(self::URI + '//read//' + $this->getInput('n'));
 
         /*foreach($html->find('a', 0) as $item){
             $items = array();
