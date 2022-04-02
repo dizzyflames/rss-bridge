fe7929df8d7af2db26b18a6d2429ae9c3aeb727d
Jason Ho <dizzyflames@hotmail.com>
2022-04-02 01:23:03 -0400

    testing asurascans instead

--- bridges/WebtoonBridge.php
+++ bridges/WebtoonBridge.php
@@ -2,7 +2,8 @@
     // work in progress
 class WebtoonBridge extends BridgeAbstract {
     const NAME = 'Webtoon';
-	const URI = 'https://www.webtoon.xyz/';
+	//const URI = 'https://www.webtoon.xyz/';
+    const URI = 'https://www.asurascans.com/';
 	const DESCRIPTION = 'Returns all chapters to date';
 	const MAINTAINER = 'dizzyflames';
 	const PARAMETERS = array( array(
@@ -17,7 +18,8 @@ class WebtoonBridge extends BridgeAbstract {
 
 	public function collectData() {
 		//$item = array(); // Create an empty item
-        $html = getSimpleHTMLDOM(self::URI . '//read//' . $this->getInput('n'));
+        //$html = getSimpleHTMLDOM(self::URI . '//read//' . $this->getInput('n'));
+        $html = getSimpleHTMLDOM(self::URI . '//comics//' . str_replace(' ', '-', $this->getInput('n')));
 
         foreach($html->find('a', 0) as $item){
             $items = array();
