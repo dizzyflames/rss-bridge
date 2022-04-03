961e9641fd2443a70d31c60d86f26220edf6bc54
Jason Ho <dizzyflames@hotmail.com>
2022-04-03 17:31:14 -0400

    testing using tempalte

--- bridges/WebtoonBridge.php
+++ bridges/WebtoonBridge.php
@@ -21,14 +21,17 @@ class WebtoonBridge extends BridgeAbstract {
 		//$item = array(); // Create an empty item
         //$html = getSimpleHTMLDOM(self::URI . '//read//' . $this->getInput('n'));
         //$html = getSimpleHTMLDOM(self::URI . '\/comics/' . str_replace(' ', '-', $this->getInput('n')));
-        $html = getSimpleHTMLDOM(self::URI + 'manga/' + str_replace(' ', '-', $this->getInput('n')));
+        $html = getSimpleHTMLDOM(self::URI . 'manga/' . str_replace(' ', '-', $this->getInput('n')));
 
-        foreach($html->find('ul', 0)->find('li', 0)->find('a', 0) as $item){
+        /*foreach($html->find('ul', 0)->find('li', 0)->find('a', 0) as $item){
             $items = array();
             $items['uri'] = $item->href;
             $items['title'] = $item->plaintext;
             $this->items[] = $items;
-        }
+        }*/
+        $item = array();
+        $item['title'] = 'hellow world';
+        $this->items[] = $item;
 		//$item['title'] = 'Hello World!';
 		//$this->items[] = $item; // Add item to the list
 	}
