99362c0e0d16e1e179efe2ef92fdfa2c13117ac0
Jason Ho <dizzyflames@hotmail.com>
2022-04-03 16:52:37 -0400

    trying out new website

--- bridges/WebtoonBridge.php
+++ bridges/WebtoonBridge.php
@@ -3,7 +3,8 @@
 class WebtoonBridge extends BridgeAbstract {
     const NAME = 'Webtoon';
 	//const URI = 'https://www.webtoon.xyz/';
-    const URI = 'https://www.asurascans.com/';
+    //const URI = 'https://www.asurascans.com/';
+    const URI = 'https://hentai20.com/';
 	const DESCRIPTION = 'Returns all chapters to date';
 	const MAINTAINER = 'dizzyflames';
 	const PARAMETERS = array( array(
@@ -19,7 +20,8 @@ class WebtoonBridge extends BridgeAbstract {
 	public function collectData() {
 		//$item = array(); // Create an empty item
         //$html = getSimpleHTMLDOM(self::URI . '//read//' . $this->getInput('n'));
-        $html = getSimpleHTMLDOM(self::URI . '\/comics/' . str_replace(' ', '-', $this->getInput('n')));
+        //$html = getSimpleHTMLDOM(self::URI . '\/comics/' . str_replace(' ', '-', $this->getInput('n')));
+        $html = getSimpleHTMLDOM(self::URI . 'manga/' . str_replace(' ', '-', $this->getInput('n')));
 
         foreach($html->find('a', 0) as $item){
             $items = array();
