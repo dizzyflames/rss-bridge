7a94d9e545d90f38ad7313c3bf9fe54559dca54d
Jason Ho <dizzyflames@hotmail.com>
2022-04-03 20:52:13 -0400

    test

--- bridges/WebtoonBridge.php
+++ bridges/WebtoonBridge.php
@@ -23,7 +23,7 @@ class WebtoonBridge extends BridgeAbstract {
         //$html = getSimpleHTMLDOM(self::URI . '\/comics/' . str_replace(' ', '-', $this->getInput('n')));
         $html = getSimpleHTMLDOM(self::URI . 'manga/' . str_replace(' ', '-', $this->getInput('n')));
         $num = 0;
-        foreach($html->find('ul[class=version-chap]', 0)->find('li') as $element){
+        foreach($html->find('ul.version-chap', 0)->find('li') as $element){
             foreach($element->find('a') as $element1){
                 $item = array();
                 $item['uri'] = $element1->href;
