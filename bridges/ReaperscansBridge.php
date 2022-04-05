7b8a47dea21d8a4f20cf72b4ad5c0f0bb4e8ae3d
Jason Ho <dizzyflames@hotmail.com>
2022-04-04 21:25:54 -0400

    add div to find()

--- bridges/ReaperscansBridge.php
+++ bridges/ReaperscansBridge.php
@@ -20,7 +20,7 @@ class ReaperscansBridge extends BridgeAbstract {
 	public function collectData() {
         $html = getSimpleHTMLDOM(self::URI . 'series/' . $this->getInput('n') . '/');
         $this->title = $html->find('.post-title h1', 0)->plaintext;
-        $this->icon = $html->find('div.summary_image a img', 0)[1]->src;
+        $this->icon = $html->find('div.profile-manga div.summary_image a img', 0)->src;
 
         foreach($html->find('ul.version-chap', 0)->find('li') as $element){
             $item = array();
