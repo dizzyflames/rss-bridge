048745c3b0c31842a64abef8a44a880c6c3114ca
Jason Ho <dizzyflames@hotmail.com>
2022-04-05 01:06:53 -0400

    test

--- bridges/LeviatanscansBridge.php
+++ bridges/LeviatanscansBridge.php
@@ -23,11 +23,12 @@ class LeviatanscansBridge extends BridgeAbstract {
         // no images right now issue with locating the image
         $this->icon = $html->find('div.summary_image a img', 0)->src;
 
-        foreach($html->find('ul.version-chap', 0)->find('li') as $element){
+        foreach($html->find('ul') as $element){//, 0)->find('li') as $element){
             $item = array();
             //$element1 = $element->find('a', 0);
             //$item['uri'] = $element1->href;
-            $item['title'] = 'hello';//$element1->plaintext;
+            $str = $element;
+            $item['title'] = $str;//$element1->plaintext;
             //$item['timestamp'] = strtotime($element->find('span i', 0)->plaintext);
             $this->items[] = $item;
         }
